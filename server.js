e1e3a5db8f0a2863948957633646f762fc06265a
MustiAde <adelajamustapha1@gmail.com>
2022-04-24 23:00:15 +0100

    added 404 page

--- server.js
+++ server.js
@@ -9,9 +9,21 @@ const server = http.createServer((req, res) =>{
         fs.readFile('./pages/index.html', 'utf8', function(err, data){
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
+        });
+    }
+    else if (req.url === '/about'){
+        fs.readFile('./pages/about.html', 'utf8', function(err, data){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    }
+    else {
+        fs.readFile('./pages/404.html', 'utf8', function(err, data){
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end(data);
         })
     };
     });
 
 server.listen(port, host);
-console.log(`we are live at ${host}:${port}`);
+console.log(`website is running at ${host}:${port}`);
