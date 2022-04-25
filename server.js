46677df6b0e8950c3362806e02dba7f80979ed2c
MustiAde <adelajamustapha1@gmail.com>
2022-04-25 10:16:01 +0100

    added the system route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const os = require('os');
 
 const host = '127.0.0.1';
 const port = 3000;
@@ -15,6 +16,22 @@ const server = http.createServer((req, res) =>{
         fs.readFile('./pages/about.html', 'utf8', function(err, data){
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data);
+        });
+    }
+    else if(req.url === '/sys'){
+        res.writeHead(201, {'Content-Type': 'text/plain'});
+        res.end('Your OS info has been saved successfully!');
+        const osInfo = {
+            hello_there: 'Stop peeping my specs you creep!',
+            hostname: os.hostname(),
+            platform: os.platform(),
+            architecture: os.arch(),
+            numberOfCPUS: os.cpus().length,
+            networkInterfaces: os.networkInterfaces(),
+            uptime: os.uptime()
+        };
+        fs.writeFile('./osinfo.json', JSON.stringify(osInfo), ()=>{
+            console.log('Succesfully saved client os info');
         });
     }
     else {
